e5011342bddbb8bb0f7461d79281441cf096818d
Kevin Hurley <kphurley@gmail.com>
2016-08-02 14:18:13 -0500

    test tweets working and displaying

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var swig = require('swig');
 var app = express(); // creates an instance of an express application
+
+var routes = require('./routes/');
+
+app.use(express.static('./public'));
+
+app.use('/', routes);
+
+swig.setDefaults({cache: false});
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(function (req, res, next) {
     console.log(req.method + ' ' + req.url);
     next();
 });
-app.get('/', function (req, res) {
-    res.send('Welcome to our amazing app!');
-});
-app.get('/news', function (req, res) {
-    res.send('News page');
-})
+
+
 app.listen(3000);
