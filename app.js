f9720894b550779e13f1c6a2f31b712781c5fcab
Kevin Hurley <kphurley@gmail.com>
2016-08-02 11:42:54 -0500

    Logging and package.json changes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express(); // creates an instance of an express application
 
-app.get('/', function(req, res){
+app.use(function (req, res, next) {
+    console.log(req.method + ' ' + req.url);
+    next();
+});
+app.get('/', function (req, res) {
     res.send('Welcome to our amazing app!');
 });
-
+app.get('/news', function (req, res) {
+    res.send('News page');
+})
 app.listen(3000);
