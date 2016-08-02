f1fc21c9be1589e858bbf95273fd8a00c8b76f7f
Kevin Hurley <kphurley@gmail.com>
2016-08-02 16:43:57 -0500

    App functioning with sockets

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var swig = require('swig');
 var app = express(); // creates an instance of an express application
+var socketio = require('socket.io');
 
 var routes = require('./routes/');
 
+var server = app.listen(3000);
+var io = socketio.listen(server);
+
+app.use(function (req, res, next) {
+    console.log(req.method + ' ' + req.url);
+    next();
+});
+
 app.use(express.static('./public'));
 
-app.use('/', routes);
+app.use('/', routes(io));
 
 swig.setDefaults({cache: false});
 
@@ -15,10 +24,3 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.use(function (req, res, next) {
-    console.log(req.method + ' ' + req.url);
-    next();
-});
-
-
-app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,54 @@ var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
-
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+var bodyParser = require('body-parser');
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
 });
-
-module.exports = router;
+//module.exports = router;
+module.exports = function (io) {
+    router.get('/', function (req, res) {
+        var tweets = tweetBank.list();
+        res.render('index', {
+            tweets: tweets,
+            showForm: true,
+            prePopulate: ''
+        });
+    });
+    router.get('/users/:type', function (req, res) {
+        var type = req.params.type;
+        var list;
+        if (isNaN(parseInt(type))) {
+            list = tweetBank.find({
+                name: type
+            });
+            res.render('index', {
+                tweets: list,
+                showForm: true,
+                prePopulate: type
+            });
+        }
+        else {
+            list = tweetBank.find({
+                id: parseInt(type)
+            });
+            res.render('index', {
+                tweets: list,
+                showForm: false
+            });
+        }
+    });
+    router.post('/tweets', urlencodedParser, function (req, res) {
+        var name = req.body.name;
+        var text = req.body.text;
+        tweetBank.add(name, text);
+        var tweet = tweetBank.find({name: name, content: text});
+        console.log(tweet);
+        io.sockets.emit('newTweet', {name: tweet[0].name, content: tweet[0].content, id: tweet[0].id});
+        res.redirect('/');
+    });
+    return router;
+};
